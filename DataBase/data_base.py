eedffff78e75a355e40724000b9aecd7dd228da9
Стоун <17th@inbox.ru>
2023-05-02 12:54:26 +0300

    DPB 0.9a 02/05 12:00

--- DataBase/data_base.py
+++ DataBase/data_base.py
@@ -3,7 +3,7 @@ import sqlite3
 
 class DataBase:
 
-    def __init__(self, db_path: str = 'DataBase/DP_bot_db.db'):
+    def __init__(self, db_path: str = 'DataBase/dirty_db.db'):
         self.db_path = db_path
 
     @property
